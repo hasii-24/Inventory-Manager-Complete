d0f72b85fc473003fddd0e6eeb5a66da7e82140a
Hasitha Goda <godahasitha@gmail.com>
2025-07-24 07:04:26 +0530

    Update server.js

--- Inventory-Manager-Complete/backend/server.js
+++ Inventory-Manager-Complete/backend/server.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
 
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB error:', err));
