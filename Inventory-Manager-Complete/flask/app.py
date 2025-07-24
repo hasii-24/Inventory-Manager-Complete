986b46f5db85e984c6cdca61bac04f1ad2e3aab9
Hasitha Goda <godahasitha@gmail.com>
2025-07-24 11:33:04 +0530

    Update app.py

--- Inventory-Manager-Complete/flask/app.py
+++ Inventory-Manager-Complete/flask/app.py
@@ -50,5 +50,8 @@ def predict_stock():
 
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env var
+    app.run(host="0.0.0.0", port=port, debug=True)
+
