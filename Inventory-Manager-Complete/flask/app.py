ec096889ffec9192947e7ef1417b676f212c39f6
Hasitha Goda <godahasitha@gmail.com>
2025-07-24 10:48:21 +0530

    Update app.py

--- Inventory-Manager-Complete/flask/app.py
+++ Inventory-Manager-Complete/flask/app.py
@@ -4,23 +4,18 @@ import pymongo
 from bson import ObjectId
 import os
 
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
-client = pymongo.MongoClient(MONGO_URI)
-
 app = Flask(__name__)
 CORS(app)
 
-
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["inventoryDB"]
 products = db["products"]
 orders = db["orders"]
 
 @app.route('/predict', methods=['GET'])
 def predict_stock():
-    # Step 1: Get all products
     products = list(db.products.find({}, {"_id": 0, "name": 1, "quantity": 1}))
 
-    # Step 2: Aggregate purchases based on productName
     pipeline = [
         {"$group": {
             "_id": "$productName",
@@ -29,10 +24,8 @@ def predict_stock():
     ]
     purchases = list(db.orders.aggregate(pipeline))
 
-    # Step 3: Map productName to totalPurchased
     purchased_map = {p["_id"]: p["totalPurchased"] for p in purchases}
 
-    # Step 4: Combine product data with purchased data
     result = []
     for product in products:
         name = product["name"]
@@ -46,5 +39,7 @@ def predict_stock():
         })
 
     return jsonify(result)
+
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
